e122f9fffe7cf491401a15f61df2a164e1c327c5
Carlos-MKR <saavedracarlos0066@gmail.com>
2026-09-01 16:43:16 -0600

    feat: configure environment variables, initialize Prisma client, and update TypeScript configuration for ESM compatibility

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -7,5 +7,5 @@ import env from 'env-var';
 export const envs = {
     PORT: env.get('PORT').required().asPortNumber(),
     PUBLIC_PATH: env.get('PUBLIC_PATH').default('public').asString(),
-    POSTGRES_URL: env.get('POSTGRES_URL').required().asString(),
+    DATABASE_URL: env.get('DATABASE_URL').required().asString(),
 };
--- src/data/postgres/index.ts
+++ src/data/postgres/index.ts
@@ -3,7 +3,7 @@ import { PrismaPg } from "@prisma/adapter-pg";
 import { envs } from "../../config/envs.js";
 
 const adapter = new PrismaPg({
-    connectionString: envs.POSTGRES_URL,
+    connectionString: envs.DATABASE_URL,
 });
 
 export const prisma = new PrismaClient({ adapter });
